938d403f5697f2d089e986ac0e513c0e0c2548c1
Jamshadj <jamshadalij2004@gmail.com>
2025-04-21 11:37:23 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+import HeroBanner from "./components/HeroBanner/HeroBanner";
+
+
+
+
+export default function Home() {
+  return (
+    <section className="overflow-hidden">
+      <HeroBanner/>
+    
+    </section>
+  );
+}
