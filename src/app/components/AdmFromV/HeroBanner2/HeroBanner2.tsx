4cba1dbda1cc7b76fc9725fa271e2fb4495ae2b6
sidharthmk24 <sidharthmk24@gmail.com>
2025-07-22 09:38:31 +0530

    old content

--- src/app/components/AdmFromV/HeroBanner2/HeroBanner2.tsx
+++ src/app/components/AdmFromV/HeroBanner2/HeroBanner2.tsx
@@ -36,7 +36,7 @@ declare global {
   }
 }
 
-const HeroBanner2 = () => {
+const HeroBanner = () => {
   const {
     register,
     handleSubmit,
@@ -118,7 +118,7 @@ const HeroBanner2 = () => {
       {/* Main Content with Background Image */}
       <main className="relative min-h-screen flex  items-center justify-center">
         {/* Background Image Carousel */}
-        {/* <div className="absolute inset-0 z-0">
+        <div className="absolute inset-0 z-0">
           <Swiper
             className={`transition-all duration-700 ease-in-out w-full h-full`}
             spaceBetween={10}
@@ -133,7 +133,7 @@ const HeroBanner2 = () => {
             modules={[Autoplay]}
           >
             <SwiperSlide>
-              <div className="relative h-[50vh] md:h-[93vh] w-full">
+              <div className="relative h-[50vh] md:h-[89vh] w-full">
                 <Image
                   src={websitebg1}
                   alt="Banner1"
@@ -145,7 +145,7 @@ const HeroBanner2 = () => {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="relative h-[50vh] md:h-[93vh] w-full">
+              <div className="relative h-[50vh] md:h-[89vh] w-full">
                 <Image
                   src={websitebg2}
                   alt="Banner2"
@@ -157,7 +157,7 @@ const HeroBanner2 = () => {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="relative h-[50vh] md:h-[93vh] w-full">
+              <div className="relative h-[50vh] md:h-[89vh] w-full">
                 <Image
                   src={websitebg3}
                   alt="Banner3"
@@ -169,7 +169,7 @@ const HeroBanner2 = () => {
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className="relative h-[50vh] md:h-[93vh] w-full">
+              <div className="relative h-[50vh] md:h-[89vh] w-full">
                 <Image
                   src={websitebg4}
                   alt="Banner3"
@@ -182,162 +182,196 @@ const HeroBanner2 = () => {
             </SwiperSlide>
             <div className="swiper-pagination" />
           </Swiper>
-        </div> */}
+        </div>
 
         {/* Form Container - Centered */}
-<div
- className="w-full min-h-screen px-1 sm:px-6 md:px-8 py-16 flex justify-center items-center bg-cover bg-center relative mt-[1rem] sm:mt-0 "  
- style={{
-    backgroundImage: "url('/your-banner-image.jpg')", // 👈 Replace with your actual image path
-  }}
->
-  {/* Decorative Element - Top Right */}
-  <motion.div
-    initial={{ opacity: 0, scale: 0.8 }}
-    whileInView={{ opacity: 1, scale: 1 }}
-    transition={{ duration: 0.5, delay: 0.2 }}
-    viewport={{ once: true }}
-    className="absolute top-[10rem] right-[0rem] sm:top-6 sm:right-6 md:top-[0rem] md:right-[2.5rem] lg2:top-[-3rem] lg2:right-[-2rem] w-20 h-20 md:w-24 md:h-24 lg2:w-36 lg2:h-36 bg-green-400 rounded-bl-full rounded-tl-full rounded-br-full opacity-100 z-10"
-  />
+        <div className="relative w-[100vw] mx-auto mt-[30rem] md:mt-16 px-4 md:min-h-[90vh] flex justify-center md:justify-end items-start z-10">
+          {/* Decorative Elements with Animations */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            viewport={{ once: true }}
+            className="absolute top-[-1rem] right-[-1rem] w-20 h-20 md:top-[-2rem] md:right-[-1rem] lg2:top-[-3rem] lg2:right-[-2rem] lg2:w-36 lg2:h-36 bg-green-400 rounded-bl-full rounded-tl-full rounded-br-full opacity-100"
+          ></motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            viewport={{ once: true }}
+            className="absolute bottom-[-20] left-[-2rem] md:top-[440] md:left-[210] lg2:left-[65%] !lg2:bottom-[10%]  w-24 h-12 md:w-32 md:h-16 xl:w-36 xl:h-18 xl:bottom-[13rem] xl:left-[65%] bg-[#FEA3CA] rotate-220 rounded-t-full opacity-100"
+          ></motion.div>
 
-  {/* Decorative Element - Bottom Left */}
-  <motion.div
-    initial={{ opacity: 0, scale: 0.8 }}
-    whileInView={{ opacity: 1, scale: 1 }}
-    transition={{ duration: 0.5, delay: 0.4 }}
-    viewport={{ once: true }}
-    className="absolute bottom-[14.5rem] left-[-1.2rem] md:bottom-15 md:left-5 lg2:left-[65%] lg2:bottom-[10%] w-24 h-12 md:w-32 md:h-16 xl:w-36 xl:h-18 bg-[#FEA3CA] rotate-[220deg] rounded-t-full opacity-100 z-10"
-  />
-
-  {/* Form Card */}
-<motion.div
-  ref={formRef}
-  initial={{ opacity: 0, y: 20 }}
-  animate={{ opacity: isFormInView ? 1 : 0, y: isFormInView ? 0 : 20 }}
-  transition={{ duration: 0.5 }}
-  className="bg-white  shadow-2xl rounded-2xl w-full max-w-[calc(100%-2rem)] sm:max-w-[600px] lg:max-w-[calc(100%-6rem)] px-8 sm:px-8 lg:px-12 py-12 md:py-20 md:mt-[-4rem] mt-[-5rem] relative z-20"
->
-    {/* Headings */}
-    <motion.h2
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      className="text-2xl sm:text-3xl lg2:text-[1.8rem] xl:text-[3.5rem] leading-tight font-bold text-[#FB7824] text-center"
-      style={{ fontFamily: "Helvetica, Arial, sans-serif" }}
-    >
-      Admissions Open
-    </motion.h2>
-    <motion.h2
-      initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      className="text-xl sm:text-2xl lg2:text-[1.5rem] xl:text-[2.5rem] leading-tight pb-4 font-bold text-gray-800 text-center"
-      style={{ fontFamily: "Helvetica, Arial, sans-serif" }}
-    >
-      Now For 2025
-    </motion.h2>
-
-  <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-6 text-black">      {/* Student Name */}
-      <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.1 }}>
-        <input
-          type="text"
-          required
-          placeholder="Student Name"
-          aria-label="Student Name"
-          {...register("studentName", { required: "Student Name is required" })}
-          className="w-full border-b border-black/[20%] focus:outline-none text-base md:text-lg2 xl:text-xl"
-        />
-        {errors.studentName && <p className="text-red-500 text-xs md:text-sm">{errors.studentName.message}</p>}
-      </motion.div>
-
-      {/* Parent Email */}
-      <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.2 }}>
-        <input
-          type="email"
-          required
-          placeholder="Parent Email"
-          aria-label="Parent Email"
-          {...register("parentEmail", { required: "Parent Email is required" })}
-          className="w-full border-b border-black/[20%] focus:outline-none text-base md:text-lg2 xl:text-xl"
-        />
-        {errors.parentEmail && <p className="text-red-500 text-xs md:text-sm">{errors.parentEmail.message}</p>}
-      </motion.div>
-
-      {/* Parent Phone */}
-      <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.3 }}>
-        <input
-          type="tel"
-          placeholder="Parent Phone"
-          required
-          aria-label="Parent Phone"
-          {...register("parentPhone", {
-            required: "Phone Number is required",
-            pattern: {
-              value: /^[0-9]{10}$/,
-              message: "Phone Number must be exactly 10 digits"
-            }
-          })}
-          className="w-full border-b border-black/[20%] focus:outline-none text-base md:text-lg2 xl:text-xl"
-        />
-        {errors.parentPhone && <p className="text-red-500 text-xs md:text-sm">{errors.parentPhone.message}</p>}
-      </motion.div>
-
-      {/* Class Dropdown */}
-      <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.4 }}>
-        <label className="block text-sm text-gray-600">
-          <select
-            {...register("class", { required: "Class is required" })}
-            defaultValue=""
-            aria-label="class"
-            className="w-full border-b border-black/[20%] focus:outline-none bg-transparent text-black/70 placeholder:text-black/70 text-base md:text-lg2 xl:text-xl appearance-none"
+          {/* Form Card with Scroll-Triggered Animation */}
+          <motion.div
+            ref={formRef}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: isFormInView ? 1 : 0, y: isFormInView ? 0 : 20 }}
+            transition={{ duration: 0.5 }}
+            className="bg-white rounded-lg2 shadow-lg2 px-4 md:px-6 lg2:px-8 py-6  w-full 
+                       max-w-[95vw] sm:max-w-[80vw] md:max-w-[60vw] lg2:max-w-[30vw] xl:max-w-[30vw] 
+                       my-0 relative"
           >
-            <option value="" disabled className="!text-gray-400 text-base md:text-lg2 xl:text-xl">
-              Class Being Applied For
-            </option>
-            <option className="text-black" value="V">V</option>
-            <option className="text-black" value="VI">VI</option>
-            <option className="text-black" value="VII">VII</option>
-            <option className="text-black" value="VIII">VIII</option>
-            <option className="text-black" value="IX">IX</option>
-            <option className="text-black" value="X">X</option>
-            <option className="text-black" value="XI">XI</option>
-            <option className="text-black" value="XII">XII</option>
-          </select>
-        </label>
-        {errors.class && <p className="text-red-500 text-xs md:text-sm">{errors.class.message}</p>}
-      </motion.div>
+            {/* Animated Heading */}
+            <motion.h2
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+              className="text-2xl md:text-3xl lg2:text-[1.8rem] xl:text-[3.5rem]  leading-tight 
+                         font-bold  text-[#FB7824] text-center"
+              style={{ fontFamily: "Helvetica, Arial, sans-serif" }}
+            >
+              Admissions Open
+            </motion.h2>
+            <motion.h2
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+              className="text-2xl md:text-3xl lg2:text-[1.5rem] xl:text-[2.5rem] leading-tight 
+                         pb-4 lg2:pb-5  font-bold text-gray-800 text-center"
+              style={{ fontFamily: "Helvetica, Arial, sans-serif" }}
+            >
+              Now For 2025
+            </motion.h2>
 
-      {/* Location */}
-      <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.1 }}>
-        <input
-          type="text"
-          required
-          placeholder="Student Name"
-          aria-label="Student Name"
-          {...register("studentName", { required: "Student Name is required" })}
-          className="w-full border-b border-black/[20%] focus:outline-none text-base sm:text-lg xl:text-xl"
-        />
-        {errors.studentName && <p className="text-red-500 text-xs sm:text-sm">{errors.studentName.message}</p>}
-      </motion.div>
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 md:space-y-8 text-black">
+              {/* Student Name */}
+              <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.1 }}>
+                <input
+                  type="text"
+                  required
+                  placeholder="Student Name"
+                  aria-label="Student Name"
+                  {...register("studentName", { required: "Student Name is required" })}
+                  className="w-full border-b border-black/[20%] focus:outline-none text-base md:text-lg2 xl:text-xl"
+                />
+                {errors.studentName && <p className="text-red-500 text-xs md:text-sm">{errors.studentName.message}</p>}
+              </motion.div>
 
-      {/* Submit */}
-      <div className="justify-center flex">
-        <button
-          type="submit"
-          className="w-full bg-[#FB7824] hover:bg-[#e7681e] cursor-pointer text-white py-2 px-4 rounded-md flex justify-center items-center transition-all"
-          disabled={loading}
-        >
-          {loading ? <ClipLoader color="#fff" size={20} /> : "Submit"}
-        </button>
-      </div>
-    </form>
-    {/* FORM END */}
-  </motion.div>
-</div>
+              {/* Parent Email */}
+              <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.2 }}>
+                <input
+                  type="email"
+                  required
+                  placeholder="Parent Email"
+                  aria-label="Parent Email"
+                  {...register("parentEmail", { required: "Parent Email is required" })}
+                  className="w-full border-b border-black/[20%] focus:outline-none text-base md:text-lg2 xl:text-xl"
+                />
+                {errors.parentEmail && <p className="text-red-500 text-xs md:text-sm">{errors.parentEmail.message}</p>}
+              </motion.div>
 
+              {/* Parent Phone */}
+              <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.3 }}>
+                <input
+                  type="tel"
+                  placeholder="Parent Phone"
+                  required
+                  aria-label="Parent Phone"
+                  {...register("parentPhone", {
+                    required: "Phone Number is required",
+                    pattern: {
+                      value: /^[0-9]{10}$/,
+                      message: "Phone Number must be exactly 10 digits"
+                    }
+                  })}
+                  
+                  className="w-full border-b border-black/[20%] focus:outline-none text-base md:text-lg2 xl:text-xl"
+                />
+                {errors.parentPhone && <p className="text-red-500 text-xs md:text-sm">{errors.parentPhone.message}</p>}
+              </motion.div>
+
+              {/* Class Dropdown */}
+              <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.4 }}>
+                <label className="block text-sm text-gray-600">
+                  <select
+                    {...register("class", { required: "Class is required" })}
+                    defaultValue=""
+                    aria-label="class"
+                    className="w-full border-b border-black/[20%] focus:outline-none bg-transparent text-black/70 placeholder:text-black/70 text-base md:text-lg2 xl:text-xl appearance-none"
+                  >
+                    <option value="" disabled className="!text-gray-400 text-base md:text-lg2 xl:text-xl placeholder:text-white">
+                      Class Being Applied For
+                    </option>
+                    {/* <option className="text-black" value="Nursery">
+                      Nursery
+                    </option>
+                    <option className="text-black" value="PP I">
+                      PP I
+                    </option>
+                    <option className="text-black" value="PP II">
+                      PP II
+                    </option>
+                    <option className="text-black" value="I">
+                      I
+                    </option>
+                    <option className="text-black" value="II">
+                      II
+                    </option>
+                    <option className="text-black" value="III">
+                      III
+                    </option>
+                    <option className="text-black" value="IV">
+                      IV
+                    </option> */}
+                    <option className="text-black" value="V">
+                      V
+                    </option>
+                    <option className="text-black" value="VI">
+                      VI
+                    </option>
+                    <option className="text-black" value="VII">
+                      VII
+                    </option>
+                    <option className="text-black" value="VIII">
+                      VIII
+                    </option>
+                    <option className="text-black" value="IX">
+                      IX
+                    </option>
+                    <option className="text-black" value="X">
+                      X
+                    </option>
+                    <option className="text-black" value="XI">
+                      XI
+                    </option>
+                    <option className="text-black" value="XII">
+                      XII
+                    </option>
+                  </select>
+                </label>
+                {errors.class && <p className="text-red-500 text-xs md:text-sm">{errors.class.message}</p>}
+              </motion.div>
+
+              {/* Location */}
+              <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5, delay: 0.5 }}>
+                <input
+                  type="text"
+                  placeholder="Location"
+                  aria-label="Location"
+                  required
+                  {...register("location", { required: "Location is required" })}
+                  className="w-full border-b border-black/[20%] focus:outline-none text-base md:text-lg2 xl:text-xl"
+                />
+                {errors.location && <p className="text-red-500 text-xs md:text-sm">{errors.location.message}</p>}
+              </motion.div>
+
+              {/* Submit Button */}
+              <div className="justify-center flex">
+                <button
+                  type="submit"
+                  className="w-full bg-[#FB7824] hover:bg-[#e7681e] cursor-pointer text-white py-2 px-4 rounded-md flex justify-center items-center"
+                  disabled={loading}
+                >
+                  {loading ? <ClipLoader color="#fff" size={20} /> : "Submit"}
+                </button>
+              </div>
+            </form>
+          </motion.div>
+        </div>
       </main>
     </div>
   );
 };
 
-export default HeroBanner2;
+export default HeroBanner;
